8e40076bf30d13a8798458f2d760833aab1404a6
CarsonRey <c.rey494@gmail.com>
2019-01-18 11:37:59 -0500

    hours form refactored and working, updates state

--- src/components/HoursForm.js
+++ src/components/HoursForm.js
@@ -6,39 +6,46 @@ import { connect } from 'react-redux'
 class HoursForm extends Component {
 
   state = {
-    monday: {start: "", end: ""},
-    tuesday: {start: "", end: ""},
-    wednesday: {start: "", end: ""},
-    thursday: {start: "", end: ""},
-    friday: {start: "", end: ""},
-    saturday: {start: "", end: ""},
-    sunday: {start: "", end: ""}
-    }
+     days:[{monday: {start: "", end: ""}},
+     {tuesday: {start: "", end: ""}},
+     {wednesday: {start: "", end: ""}},
+     {thursday: {start: "", end: ""}},
+     {friday: {start: "", end: ""}},
+     {saturday: {start: "", end: ""}},
+     {sunday: {start: "", end: ""}}]
+  }
 
   handleChange = (e) => {
-    console.log(this.state)
-    // console.log(e.target.name)
-    this.setState({
-      [e.target.name]: e.target.value
-    }, () => console.log(this.state) )
+    let days = this.state.days
+    let index = parseInt(e.target.dataset.id)
+    // changing the value of days
+        days[index][e.target.name][e.target.id] = e.target.value
+
+      this.setState({ days }, () => console.log(this.state))
+  }
+
+  formInputHTML = (day, index) => {
+    // debugger
+    return <div> <label for={day.toLowerCase()}>{day}: </label>
+    <input type="time" name={day.toLowerCase()} data-id={index} id="start" value={this.state.days[index][`${day.toLowerCase()}`].start || ""}/>
+    <input type="time" name={day.toLowerCase()} data-id={index} id="end" value={this.state.days[index][`${day.toLowerCase()}`].end || ""}/> <br/></div>
   }
 
   render() {
     let { createRequest, hideForm } = this.props
-
+// debugger
     let userId = parseInt(localStorage.userId)
     // let params = {mouths_to_feed: mouths, user_id: userId}
-
+    let days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
     return (
       <React.Fragment >
 
         <form onChange={(e) => this.handleChange(e)}>
 
-        <label for="monday">Monday: </label>
-        <input type="time" name="monday" value={this.state.monday.start || ""}/>
-        <input type="time" name="monday" value={this.state.monday.end || ""}/> <br/>
+        { days.map((day, index) => this.formInputHTML(day, index)) }
 
-        <label for="start">Tuesday: </label>
+
+        {/* <label for="start">Tuesday: </label>
         <input type="time" name="start" value={this.state.tuesday.start}/>
         <input type="time" name="end" value={this.state.tuesday.end}/><br/>
 
@@ -60,7 +67,7 @@ class HoursForm extends Component {
 
         <label for="start">Sunday: </label>
         <input type="time" id="start" value={this.state.sunday.start}/>
-        <input type="time" id="end" value={this.state.sunday.end}/>
+        <input type="time" id="end" value={this.state.sunday.end}/> */}
 
           <div onClick={() => console.log}>
              Add Hours
