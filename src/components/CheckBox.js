b26ea923a8aedd60c4d52d5f32893a0e1eab6a64
CarsonRey <c.rey494@gmail.com>
2019-02-19 00:26:21 -0500

    .

--- src/components/CheckBox.js
+++ src/components/CheckBox.js
@@ -3,6 +3,10 @@ const CheckBox = (props) => {
 
   return (
     <div className="checkbox checked blue" onClick={() => console.log("hi")} >
+      <div className="checkmark">
+        <div className="checkmark_stem"></div>
+        <div className="checkmark_kick"></div>
+      </div>
     </div>
   )
 }
